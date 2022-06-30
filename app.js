131f20edee5f1be80a2da5562ed4e8b95fc87ce3
bjsaimani <45102237+bjsaimani@users.noreply.github.com>
2022-06-30 15:43:31 +0530

    routing requests

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
+const { rmSync } = require('fs');
 const http=require('http');
 
 const server=http.createServer((req,res)=>{
-    console.log('jani');
-    return 'jani';
+    //requests to the server
+    //console.log(req.url,req.method,req.headers);
+    const url=req.url;
+    if(url=='/'){
+        res.write(`
+        <html>
+            <head>
+                <title>Enter the message</title>
+            </head>
+            <body>
+                <form action='message' method='POST'>
+                    <input type='text'value=''>
+                    <button type='submit'>send</button>
+                </form>
+            </body>
+        </html>
+        `);
+        return res.end();
+    }
+
+
+    //resoponses from the server
+    res.setHeader('contect-Type','text/html');
+    res.write(`
+    <html>
+        <head>
+            <title>Document</title>
+        </head>
+        <body>
+            <h2>this is body</h2><br>
+            this is paragraph heading
+        </body>
+    </html>`);
+    res.end();
 })
 
 server.listen(4000);
