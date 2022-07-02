b1db6d5e5c2c14cf6a75fdb7307b41ee8f7f0029
bjsaimani <45102237+bjsaimani@users.noreply.github.com>
2022-07-02 10:05:44 +0530

    routing the files into another files module

--- app.js
+++ app.js
@@ -1,69 +1,7 @@
 const http=require('http');
-const fs = require('fs');
 
-//reading the file 
+const callbackfun=require('./routes');
 
-
-const server=http.createServer((req,res)=>{
-
-    fs.readFile('message.txt',(err,line)=>{
-        if(err) throw err;
-        const arr=line.toString().split(',');
-        arr.forEach((value)=>{
-            res.write(`<body>${value}</body>`)
-            //console.log(value);
-        });
-    });
-
-    //requests to the server
-    //console.log(req.url,req.method,req.headers);
-    const url=req.url;
-    const method=req.method;
-    if(url=='/'){
-        res.write('<html>');
-        res.write('<head><title>Enter the message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button><body>');
-        res.write('</html>');
-        return res.end();
-    }
-
-
-    //resoponses from the server
-
-    if(url==='/message' && method==='POST') {
-        const body=[];
-        req.on('data',(chunk)=>{
-            console.log(chunk);
-            body.push(chunk);
-        })
-        return req.on('end',()=>{
-            const parsebody=Buffer.concat(body).toString();
-            console.log(parsebody);
-            const message=parsebody.split('=')[1];
-            let data=fs.createWriteStream('message.txt',{   
-                //this method allows us to store multiple 
-                //input data in a file
-                flags:'a'   // 'a' means appending (old data will be preserved)
-            });
-            data.write(message);
-            data.write(',');
-            res.statusCode=302;
-            res.setHeader('location','/');
-            return res.end();
-        })
-    }
-    //res.setHeader('Contect-Type','text/html');
-    res.write(`
-    <html>
-        <head>
-            <title>Document</title>
-        </head>
-        <body>
-            <h2>this is body</h2><br>
-            this is paragraph heading
-        </body>
-    </html>`);
-    res.end();
-})
+const server=http.createServer(callbackfun[1]);
 
 server.listen(4000);
---
+++ routes.js
@@ -0,0 +1,58 @@
+const fs=require('fs');
+
+const callbackfunction = (req,res)=> {
+    const url=req.url;
+    const method=req.method;
+    if(url=='/'){
+        res.write('<html>');
+        res.write('<head><title>Enter the message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button><body>');
+        res.write('</html>');
+        return res.end();
+    }
+    //resoponses from the server
+    if(url==='/message' && method==='POST') {
+        const body=[];
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        })
+        return req.on('end',()=>{
+            const parsebody=Buffer.concat(body).toString();
+            console.log(parsebody);
+            const message=parsebody.split('=')[1];
+            let data=fs.createWriteStream('message.txt',{   
+                //this method allows us to store multiple 
+                //input data in a file
+                flags:'a'   // 'a' means appending (old data will be preserved)
+            });
+            data.write(message);
+            data.write(',');
+            res.statusCode=302;
+            res.setHeader('location','/');
+            return res.end();
+        })
+    }
+    //res.setHeader('Contect-Type','text/html');
+    res.write(`
+    <html>
+        <head>
+            <title>Document</title>
+        </head>
+        <body>
+            <h2>this is body</h2><br>
+            this is paragraph heading
+        </body>
+    </html>`);
+    res.end();
+}
+
+//exporting the file to another file
+//module.exports = callbackfunction;  //method 1 passing function as variable 
+
+//module.exports = {              //method 2 passing object
+//    call1:callbackfunction,
+//    data:"just a data"
+//};  
+
+module.exports = ['krish',callbackfunction];  //method 3 passing array
