9b1a24d2e819fc428234b4a777aecdfae9d15a16
bjsaimani <45102237+bjsaimani@users.noreply.github.com>
2022-07-06 23:04:28 +0530

    outputing dynamic contect

--- routes/shop.js
+++ routes/shop.js
@@ -2,14 +2,18 @@ const path=require('path');
 
 const express=require('express');
 
-const admindata=require('./admin');
+const admindata=require('./admin'); //importing the admin file to show to user
 
 const router=express.Router();
 
 router.get('/', (req,res,next)=>{
     //console.log('In another middleware!');
-    //console.log('shop.js', admindata.products);
-    res.render('shop');
+    const prdcts=admindata.products;
+
+    console.log(admindata.products);
+    
+    res.render('shop',{products:prdcts,docTitle:'shop'});
+
     //res.sendFile(path.join(__dirname,'../','/views','/shop.html'));
 });
 
